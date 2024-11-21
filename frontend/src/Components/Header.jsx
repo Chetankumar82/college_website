2bc829038d16b7986b9161e86104ca00363cc063
Dilip055 <dilipsuthar055@gmail.com>
2024-11-21 19:13:45 +0530

    changes

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -34,9 +34,9 @@ const Header = () => {
                 <div className="container">
                     <div className="mid-header-inner">
                         <div className="header-logo d-lg-block">
-                            <a href="index.html">
+                            <Link to="index.html">
                                 <img src="assets/img/logo/logo-cit.png" style={{width: "350px", height: "100px"}} alt="Logo"/>
-                            </a>
+                            </Link>
                         </div>
                         <div className="category-form-wrap">
                         
@@ -61,7 +61,7 @@ const Header = () => {
                     <div className="primary-header-inner">
                         <div className="header-logo d-lg-none">
                             <Link to="/">
-                                <img src="assets/img/logo/logo-1.png" alt="Logo"/>
+                                <img src="assets/img/logo/logo-cit.png" alt="Logo"/>
                             </Link>
                         </div>
                         <div className="header-left-inner">
@@ -143,7 +143,7 @@ const Header = () => {
                                 </div>
                                 <div className="header-logo d-none d-lg-none">
                                     <a href="index.html">
-                                        <img src="assets/img/logo/logo-1.png" alt="Logo"/>
+                                        <img src="assets/img/logo/logo-cit.png" alt="Logo"/>
                                     </a>
                                 </div>
                                 <div className="header-right-item d-lg-none d-md-block">
@@ -158,6 +158,32 @@ const Header = () => {
                 </div>
             </div>
         </header>
+        <div id="popup-search-box">
+            <div className="box-inner-wrap d-flex align-items-center">
+                <form id="form" action="#" method="get" role="search">
+                    <input id="popup-search" type="text" name="s" placeholder="Type keywords here..."/>
+                </form>
+                <div className="search-close"><i className="fa-sharp fa-regular fa-xmark"></i></div>
+            </div>
+        </div>
+        
+
+        <div className="mobile-side-menu">
+            <div className="side-menu-content">
+                <div className="side-menu-head">
+                    <a href="index.html"><img src="assets/img/logo/logo-cit.png" alt="logo"/></a>
+                    <button className="mobile-side-menu-close"><i className="fa-regular fa-xmark"></i></button>
+                </div>
+                <div className="side-menu-wrap"></div>
+                <ul className="side-menu-list">
+                    <li><i className="fa-light fa-location-dot"></i>Address : <span>Amsterdam, 109-74</span></li>
+                    <li><i className="fa-light fa-phone"></i>Phone : <a href="tel:+01569896654">+01 569 896 654</a></li>
+                    <li><i className="fa-light fa-envelope"></i>Email : <a href="mailto:info@example.com">info@example.com</a></li>
+                </ul>
+            </div>
+        </div>
+       
+        <div className="mobile-side-menu-overlay"></div>
     </>
   )
 }
--- frontend/src/Components/Slider.jsx
+++ frontend/src/Components/Slider.jsx
@@ -5,40 +5,7 @@ const Slider = () => {
     <>
 
 
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="3000">
-   
-    <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    </ol>
 
-  
-    <div class="carousel-inner">
-        <div class="item active">
-            <img src="assets/img/slide1.png" alt="First slide" />
-            <div class="carousel-caption">
-                
-            </div>
-        </div>
-        <div class="item">
-            <img src="assets/img/slide2.png" alt="Second slide" />
-            <div class="carousel-caption">
-            </div>
-        </div>
-        <div class="item">
-            <img src="assets/img/slide3.png" alt="Third slide" />
-            <div class="carousel-caption">
-                
-            </div>
-        </div>
-    </div>
-
-</div>
-<div class="slider-content">
-    <h1>Welcome To CIT</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ipsum id velit dignissim efficitur. Nulla facilisi. Nulla facilisi.</p>
-</div>
 
 
     </>
